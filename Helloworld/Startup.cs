02921a1ff08889b1e4031e0d283f03ebba904a13
Bagas Try Ambodo <bagasambodo@gmail.com>
2017-01-11 13:22:51 +0700

    Add project files.

---
+++ Helloworld/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Helloworld.Startup))]
+namespace Helloworld
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
